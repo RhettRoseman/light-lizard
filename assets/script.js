f86c9b5507079663be31580822e4d49b6e80ad5e
RhettRoseman <rroseman@rcplanners.com>
2023-09-13 22:34:23 -0400

    added first test question

--- assets/script.js
+++ assets/script.js
@@ -24,59 +24,82 @@ submitButton.addEventListener('click', showResults);
 
 const questions = [
     {
-        question: ""
+        question: "Which of the following is not a data type in JavaScript? ",
         answers: {
-            A: "",
-            B: "",
-            C: "",
-            D: "",
-        }
-         
+            A: "Boolean",
+            B: "String",
+            C: "Number",
+            D: "Character"
+        },
+        correctAnswer: 'D'
     }, 
-    correctAnswer: ""
-    {
-        question: ""
-        answers: {
-            A: "",
-            B: "",
-            C: "",
-            D: "",
-        }
-         
-    }, 
-    correctAnswer: ""
-    {
-        question: ""
-        answers: {
-            A: "",
-            B: "",
-            C: "",
-            D: "",
-        }
-         
-    }, 
-    correctAnswer: ""
-    {
-        question: ""
-        answers: {
-            A: "",
-            B: "",
-            C: "",
-            D: "",
-        }
-         
-    }, 
-    correctAnswer: ""
-    {
-        question: ""
-        answers: {
-            A: "",
-            B: "",
-            C: "",
-            D: "",
-        }
-         
-    }, 
-    correctAnswer: ""
+     
 
-]
+    // {
+    //     question: "",
+    //     answers: {
+    //         A: "",
+    //         B: "",
+    //         C: "",
+    //         D: "",
+    //     }
+         
+    // }, 
+    // correctAnswer: "",
+    // {
+    //     question: ""
+    //     answers: {
+    //         A: "",
+    //         B: "",
+    //         C: "",
+    //         D: "",
+    //     }
+         
+    // }, 
+    // correctAnswer: "",
+    // {
+    //     question: "",
+    //     answers: {
+    //         A: "",
+    //         B: "",
+    //         C: "",
+    //         D: "",
+    //     }
+         
+    // }, 
+    // correctAnswer: "",
+function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+  
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+  
+        // variable to store the list of possible answers
+        const answers = [];
+  
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+  
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+  
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }]
